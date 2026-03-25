a2971dc862fc321b1b15b2c249105fd1ef78455b
ingridmoitinho <ingridmoitinho@gmail.com>
2026-03-25 10:48:30 -0300

    feat: adiciona rota de usuarios, melhora GET /posts e implementa POST /posts

--- src/app.js
+++ src/app.js
@@ -8,13 +8,27 @@ app.get("/", (req, res) => {
     res.send("<h1>Rede Social!<h1>")
 });
 
+app.get("/usuarios", async(req, res) => {
+    try {
+        const resultado = await pool.query(`
+            SELECT * FROM usuarios;
+            `);
+            res.json(resultado.rows)
+    } catch (erro) {
+        res.status(500).json({
+            erro:"Erro ao buscar dados de usuarios"})
+    }
+})
+
 // GET DOS POSTS
 
 app.get("/posts", async (req, res) => {
     try {
     const resultado = await pool.query(`
         SELECT 
+        usuarios.id,
         usuarios.nome,
+        post.titulo,
         post.conteudo,
         post.criado_em
     FROM post
@@ -26,7 +40,31 @@ app.get("/posts", async (req, res) => {
     } catch(erro) {
         res.status(500).json({erro: "Erro ao buscar postagens"});
     }
-})
-;
+});
+
+app.post("/posts", async (req, res) => {
+    try {
+        const { titulo, conteudo, usuario_id } = req.body;
+
+        const resultado = await pool.query(
+            `
+            INSERT INTO post (titulo, conteudo, usuario_id)
+            VALUES ($1, $2, $3)
+            RETURNING *
+            `,
+            [titulo, conteudo, usuario_id]
+        );
+
+        res.status(201).json({
+            mensagem: "Post criado com sucesso",
+            post: resultado.rows[0],
+        });
+
+    } catch (erro) {       
+        res.status(500).json({
+            erro: "Erro ao criar post"
+        });
+    }
+});
 
 module.exports = app;
