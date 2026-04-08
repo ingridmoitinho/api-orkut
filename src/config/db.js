7bf025e39ab7bf36433cb3b0a2845b865a16a122
ingridmoitinho <ingridmoitinho@gmail.com>
2026-04-08 10:13:39 -0300

    chore: adiciona script start e ajusta configuração SSL

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 const {Pool} = require("pg");
 
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,    
+        ssl: {
+            rejectUnauthorized: false
+        }       
 })
 
